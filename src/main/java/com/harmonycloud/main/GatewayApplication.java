b6969175f2a8f6015b56b8cb64c6697a35a1a396
dongqi <dongqi@harmonycloud.cn>
2019-03-15 16:39:59 +0800

    update

--- src/main/java/com/harmonycloud/main/GatewayApplication.java
+++ src/main/java/com/harmonycloud/main/GatewayApplication.java
@@ -2,12 +2,18 @@ package com.harmonycloud.main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan("com.harmonycloud")
-public class GatewayApplication {
+public class GatewayApplication extends SpringBootServletInitializer {
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(this.getClass());
+    }
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class, args);
     }
